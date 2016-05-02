c4a20c5c90e99cfa3e7b11b74e249db751a15034
zecy <angelszc163@gmail.com>
2016-05-02 16:24:58 +0800

    change the table to anime_origianl_work_support

--- app/AnimeOriginalWork.php
+++ app/AnimeOriginalWork.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class AnimeOriginalWork extends Model
 {
-    protected $table      = 'anime_original_work';
+    protected $table      = 'anime_original_work_support';
     protected $primaryKey = 'ori_id';
     protected $visible = array('ori_id', 'ori_pid', 'ori_catalog', 'haschild','ori_level','multiple');
 }

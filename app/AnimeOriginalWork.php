0c3735d81c1e6073fb36eec97b78882a2c92c669
zecy <angelszc163@gmail.com>
2016-05-11 23:28:36 +0800

    Original Works Model

---
+++ app/AnimeOriginalWork.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AnimeOriginalWork extends Model
+{
+    protected $table = 'anime_original_work';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'anime_id',
+        'ori_id',
+        'ori_pid',
+        'lv',
+        'haschild',
+        'multiple'
+    ];
+}

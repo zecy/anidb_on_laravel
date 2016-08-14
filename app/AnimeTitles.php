71099341213803a1949f5a1f85934845f0ede8bf
zecy <angelszc163@gmail.com>
2016-08-14 17:01:46 +0800

    和 AnimeBasicData 建立关联

--- app/AnimeTitles.php
+++ app/AnimeTitles.php
@@ -15,4 +15,9 @@ class AnimeTitles extends Model
                              'description',
                              'order_index'
     ];
+
+    public function basicData() {
+        // 连接到 AnimeBasicData, 第一个 anime_id 是 Title 的 anime_id, 第二个 anime_id 是 AnimeBasicData 的主键.
+        return $this->belongsTo('App\AnimeBasicData', 'anime_id', 'anime_id');
+    }
 }

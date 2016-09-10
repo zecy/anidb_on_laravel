6a96e848d95c51b3e29b6c78057b70efe6ffe105
zecy <angelszc163@gmail.com>
2016-09-10 23:29:11 +0800

    增加 short 项

--- app/AnimeOriginalWorkSupport.php
+++ app/AnimeOriginalWorkSupport.php
@@ -8,7 +8,15 @@ class AnimeOriginalWorkSupport extends Model
 {
     protected $table      = 'anime_original_work_support';
     protected $primaryKey = 'ori_id';
-    protected $visible = array('ori_id', 'ori_pid', 'ori_catalog', 'haschild','ori_level','multiple_children', 'multiple_selected');
+    protected $visible = array('ori_id',
+                               'ori_pid',
+                               'ori_catalog',
+                               'shortcut',
+                               'haschild',
+                               'ori_level',
+                               'multiple_children',
+                               'multiple_selected'
+    );
 
     public function oriworks() {
         return $this->hasMany('\App\AnimeOriginalWork', 'ori_id', 'ori_id');

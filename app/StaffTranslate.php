57431771a3bf9bb33f5df9ec720cc258eed7fa49
zecy <angelszc163@gmail.com>
2016-09-12 00:50:49 +0800

    用于翻译 staff 的 model。

---
+++ app/StaffTranslate.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StaffTranslate extends Model
+{
+    protected $table      = 'anime_staff_translate';
+    protected $primaryKey = 'id';
+}
